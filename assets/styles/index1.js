8738935dbb678d4313d243632f85d9a5c27ec8c7
syamalatha gudepu <syama8031997@gmail.com>
2026-06-03 07:20:36 +0530

    prommise chaining

--- assets/styles/index1.js
+++ assets/styles/index1.js
@@ -1,42 +1,47 @@
-function first(callback) {
-    console.log("starting first...");
+function first() {
+  return new Promise((resolve) =>{
+      console.log("starting first...");
 // wait for 2 sec
 
 setTimeout(() => {
 console.log("ending first...")
-callback()
+resolve()
 }, 2000);
+
+  })
 
 }
 
-function second(callback) {
-    console.log("starting second...");
+function second() {
+    new Promise((resolve)=>{
+        console.log("starting second...");
 // wait for 2 sec
 
 setTimeout(() => {
 console.log("ending second...")
-callback()
+resolve();
 }, 3000);
-
-}
-
-function third(callback) {
-    console.log("starting third...");
-// wait for 2 sec
-
-setTimeout((resolve) => {
-console.log("ending third...")
-callback(resolve)
-}, 5000);
-
-}
-
-first(() => {
-    second ( () => {
-        third ( ()=>{
-        console.log("finished executing all");
-
-        })
     })
 
+}
+
+function third() {
+   new Promise((resolve)=> {
+     console.log("starting third...");
+// wait for 2 sec
+
+setTimeout(() => {
+console.log("ending third...")
+resolve();
+}, 5000);
+
 })
+
+}
+
+first()
+    .then(second)
+    .then(third)
+    .then(()=>{
+        console.log("finished executing all functions")
+    })
